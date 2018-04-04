a536371dc155dacfca0ffbfb557a56b98fc5ad2a
Jonathan Turner <jonathan.d.turner@gmail.com>
2018-04-05 11:04:01 +1200

    c codegen refactoring for side effects

--- src/compile.rs
+++ src/compile.rs
@@ -1,6 +1,37 @@
 use bytecode::{Bytecode, BytecodeEngine, Fun, Ty};
 use time::PreciseTime;
 
+struct CFile {
+    output_src: String,
+    expression_stack: Vec<String>,
+}
+
+impl CFile {
+    fn new() -> CFile {
+        CFile {
+            output_src: String::new(),
+            expression_stack: vec![],
+        }
+    }
+
+    fn codegen_raw(&mut self, output: &str) {
+        self.output_src += output;
+    }
+
+    fn delay_expr(&mut self, expr: String) {
+        self.expression_stack.push(expr);
+    }
+
+    fn codegen_stmt(&mut self, stmt: &str) {
+        for expr in self.expression_stack.drain(0..) {
+            self.output_src += &expr;
+            self.output_src += ";\n";
+        }
+
+        self.output_src += stmt;
+    }
+}
+
 fn codegen_type(ty: &Ty) -> String {
     let codegen_ty = match ty {
         Ty::U64 => "unsigned long long".into(),
@@ -17,135 +48,119 @@ fn codegen_fn_header(fn_name: &str, fun: &Fun) -> String {
     format!("{} {}();\n", codegen_type(&fun.return_ty), fn_name)
 }
 
-fn codegen_fn(bc: &BytecodeEngine, fn_name: &str, fun: &Fun) -> String {
-    let mut output = String::new();
-
-    //TODO: we may want to add types in the future
-    //let mut expression_stack: Vec<(String, Ty)> = vec![];
-    let mut expression_stack: Vec<String> = vec![];
-
+fn codegen_fn(cfile: &mut CFile, bc: &BytecodeEngine, fn_name: &str, fun: &Fun) {
     let mut next_temp_id = 0;
 
     //TODO: This isn't the best solution, but it's an experiment
     let mut temp_id_stack = vec![];
 
-    output += &codegen_type(&fun.return_ty);
-    output += " ";
-    output += fn_name;
-    output += "(";
+    cfile.codegen_raw(&format!("{} {}(", &codegen_type(&fun.return_ty), fn_name));
 
     let mut first = true;
     for param in &fun.params {
-        output += &format!(
+        cfile.codegen_raw(&format!(
             "{}{} {}",
             if !first { ", " } else { "" },
             codegen_type(&param.ty),
             param.name
-        );
+        ));
         first = false;
     }
 
-    output += ") {\n";
+    cfile.codegen_raw(") {\n");
 
     for param in &fun.params {
-        output += &format!(
+        cfile.codegen_raw(&format!(
             "{} v{} = {};\n",
             codegen_type(&param.ty),
             param.var_id,
             param.name
-        );
+        ));
     }
 
     for code in &fun.bytecode {
         match code {
             Bytecode::ReturnVoid => {
-                // if we have an outstanding expression (likely void-valued), let the side-effects run
-                // all the remaining expressions should be in the most compact form
-                for expr in expression_stack {
-                    output += &format!("{};\n", expr);
-
-                }
-                output += "return;\n";
+                cfile.codegen_stmt("return;\n");
                 break;
             }
             Bytecode::ReturnLastStackValue => {
-                let retval = expression_stack.pop().unwrap();
-                // if we have an outstanding expression (likely void-valued), let the side-effects run
-                // all the remaining expressions should be in the most compact form
-                for expr in expression_stack {
-                    output += &format!("{};\n", expr);
-
-                }
-                output += &format!("return {};\n", retval);
+                let retval = cfile.expression_stack.pop().unwrap();
+                cfile.codegen_stmt(&format!("return {};\n", retval));
                 break;
             }
             Bytecode::PushU64(val) => {
-                expression_stack.push(val.to_string());
+                cfile.delay_expr(val.to_string());
             }
             Bytecode::PushU32(val) => {
-                expression_stack.push(val.to_string());
+                cfile.delay_expr(val.to_string());
             }
             Bytecode::PushBool(val) => {
-                expression_stack.push(val.to_string());
+                cfile.delay_expr(val.to_string());
             }
             Bytecode::Add => {
-                let rhs = expression_stack.pop().unwrap();
-                let lhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
+                let lhs = cfile.expression_stack.pop().unwrap();
 
-                expression_stack.push(format!("({}+{})", lhs, rhs));
+                cfile.delay_expr(format!("({}+{})", lhs, rhs));
             }
             Bytecode::Sub => {
-                let rhs = expression_stack.pop().unwrap();
-                let lhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
+                let lhs = cfile.expression_stack.pop().unwrap();
 
-                expression_stack.push(format!("({}-{})", lhs, rhs));
+                cfile.delay_expr(format!("({}-{})", lhs, rhs));
             }
             Bytecode::Mul => {
-                let rhs = expression_stack.pop().unwrap();
-                let lhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
+                let lhs = cfile.expression_stack.pop().unwrap();
 
-                expression_stack.push(format!("({}*{})", lhs, rhs));
+                cfile.delay_expr(format!("({}*{})", lhs, rhs));
             }
             Bytecode::Div => {
-                let rhs = expression_stack.pop().unwrap();
-                let lhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
+                let lhs = cfile.expression_stack.pop().unwrap();
 
-                expression_stack.push(format!("({}/{})", lhs, rhs));
+                cfile.delay_expr(format!("({}/{})", lhs, rhs));
             }
             Bytecode::Lt => {
-                let rhs = expression_stack.pop().unwrap();
-                let lhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
+                let lhs = cfile.expression_stack.pop().unwrap();
 
-                expression_stack.push(format!("({} < {})", lhs, rhs));
+                cfile.delay_expr(format!("({} < {})", lhs, rhs));
             }
             Bytecode::VarDecl(var_id) => {
                 let var = &fun.vars[*var_id];
-                let rhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
 
-                output += &format!("{} v{} = {};\n", codegen_type(&var.ty), *var_id, rhs);
+                cfile.codegen_stmt(&format!(
+                    "{} v{} = {};\n",
+                    codegen_type(&var.ty),
+                    *var_id,
+                    rhs
+                ));
             }
             Bytecode::VarDeclUninit(var_id) => {
                 let var = &fun.vars[*var_id];
 
-                output += &format!("{} v{};\n", codegen_type(&var.ty), *var_id);
+                cfile.codegen_stmt(&format!("{} v{};\n", codegen_type(&var.ty), *var_id));
             }
             Bytecode::Var(var_id) => {
-                expression_stack.push(format!("v{}", var_id));
+                cfile.delay_expr(format!("v{}", var_id));
             }
             Bytecode::Assign(var_id) => {
-                let rhs = expression_stack.pop().unwrap();
+                let rhs = cfile.expression_stack.pop().unwrap();
 
-                output += &format!("v{} = {};\n", *var_id, rhs);
+                cfile.codegen_stmt(&format!("v{} = {};\n", *var_id, rhs));
             }
             Bytecode::Call(fn_name) => {
                 let fun = bc.get_fn(fn_name);
                 let mut expr_string = String::new();
 
                 expr_string += &format!("{}(", fn_name);
-                let expression_stack_len = expression_stack.len();
+                let expression_stack_len = cfile.expression_stack.len();
                 let mut offset = fun.params.len();
                 while offset > 0 {
-                    expr_string += &expression_stack[expression_stack_len - offset];
+                    expr_string += &cfile.expression_stack[expression_stack_len - offset];
                     if offset > 1 {
                         expr_string += ", "
                     }
@@ -153,76 +168,81 @@ fn codegen_fn(bc: &BytecodeEngine, fn_name: &str, fun: &Fun) -> String {
                 }
 
                 expr_string += ")";
-                expression_stack.push(expr_string);
+                cfile.delay_expr(expr_string);
             }
             Bytecode::If(_, ty) => {
-                let cond = expression_stack.pop().unwrap();
+                //TODO using codegen_raw here is probably not correct, but if we don't we run
+                //into issues with if expressions
+
+                let cond = cfile.expression_stack.pop().unwrap();
 
                 match ty {
                     Ty::U64 | Ty::Bool | Ty::U32 | Ty::UnknownInt => {
-                        output += &format!("{} t{};\n", codegen_type(ty), next_temp_id);
+                        cfile.codegen_raw(&format!("{} t{};\n", codegen_type(ty), next_temp_id));
                         temp_id_stack.push(next_temp_id);
                         next_temp_id += 1;
                     }
                     _ => {}
                 }
 
-                output += &format!("if ({}) {{\n", cond);
+                cfile.codegen_raw(&format!("if ({}) {{\n", cond));
             }
             Bytecode::Else(_, ty) => {
                 if *ty != Ty::Void {
-                    let result = expression_stack.pop().unwrap();
-                    output += &format!("t{} = {};\n", temp_id_stack.last().unwrap(), result);
+                    let result = cfile.expression_stack.pop().unwrap();
+                    cfile.codegen_raw(&format!(
+                        "t{} = {};\n",
+                        temp_id_stack.last().unwrap(),
+                        result
+                    ));
                 }
-                output += "} else {\n";
+                cfile.codegen_raw("} else {\n");
             }
             Bytecode::EndIf(ty) => {
                 if *ty != Ty::Void {
-                    let result = expression_stack.pop().unwrap();
+                    let result = cfile.expression_stack.pop().unwrap();
                     let temp_id = temp_id_stack.pop().unwrap();
-                    output += &format!("t{} = {};\n", temp_id, result);
-                    expression_stack.push(format!("t{}", temp_id));
+                    cfile.codegen_raw(&format!("t{} = {};\n", temp_id, result));
+                    cfile.expression_stack.push(format!("t{}", temp_id));
                 }
-                output += "}\n";
+                cfile.codegen_raw("}\n");
             }
             Bytecode::BeginWhile => {
-                output += "while(1) {\n";
+                cfile.codegen_stmt("while(1) {\n");
             }
             Bytecode::WhileCond(_) => {
-                let cond = expression_stack.pop().unwrap();
-                output += &format!("if (!({})) break;\n", cond);
+                let cond = cfile.expression_stack.pop().unwrap();
+                cfile.codegen_stmt(&format!("if (!({})) break;\n", cond));
             }
             Bytecode::EndWhile(_) => {
-                output += "}\n";
+                cfile.codegen_stmt("}\n");
             }
             Bytecode::DebugPrint => {
-                let val = expression_stack.pop().unwrap();
-                expression_stack.push(format!("printf(\"DEBUG: %u\\n\", ({}));\n", val));
+                let val = cfile.expression_stack.pop().unwrap();
+                cfile.codegen_stmt(&format!("printf(\"DEBUG: %u\\n\", ({}));\n", val));
             }
-
         }
     }
 
-    output += "};\n";
-    output
+    cfile.codegen_stmt("};\n");
 }
 
 fn codegen_c_from_bytecode(bc: &BytecodeEngine) -> String {
-    let mut output = String::new();
+    let mut cfile = CFile::new();
 
-    output += "#include <stdio.h>\n";
-    output += "#include <stdbool.h>\n";
+    cfile.codegen_raw("#include <stdio.h>\n");
+    cfile.codegen_raw("#include <stdbool.h>\n");
 
     for fn_name in bc.processed_fns.keys() {
         let fun = bc.get_fn(fn_name);
-        output += &codegen_fn_header(fn_name, fun);
+        cfile.codegen_raw(&codegen_fn_header(fn_name, fun));
     }
     for fn_name in bc.processed_fns.keys() {
         let fun = bc.get_fn(fn_name);
-        output += &codegen_fn(bc, fn_name, fun);
+        &codegen_fn(&mut cfile, bc, fn_name, fun);
     }
 
-    output
+    cfile.output_src
 }
 
 pub fn compile_bytecode(bc: &BytecodeEngine, input_fname: &str) -> ::std::io::Result<String> {
