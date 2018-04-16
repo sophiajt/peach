cfff87ed1735b33a20b64b91a0b170fc43886a68
Jonathan Turner <jonathan.d.turner@gmail.com>
2018-04-16 15:38:40 +1200

    Fix sdl build for linux

--- examples/sdl/build.rs
+++ examples/sdl/build.rs
@@ -1,4 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-lib=static=SDL2main");
-    println!("cargo:rustc-link-lib=static=SDL2");
+    let target = env::var("TARGET").unwrap();
+    let target_os = target.as_str().splitn(3, "-").nth(2).unwrap();
+
+    if target_os.contains("windows") {
+        println!("cargo:rustc-link-lib=static=SDL2main");
+        println!("cargo:rustc-link-lib=static=SDL2");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=SDL2main");
+        println!("cargo:rustc-link-lib=dylib=SDL2");
+    }
 }
